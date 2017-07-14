e37cb603ebdec50329dbca126316e11525134491
Artem Artemev <art.art.v@gmail.com>
2017-07-14 23:15:43 +0100

    Build and run scripts

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script to build all components from scratch, using the maximum available CPU power
+#
+# Given parameters are passed over to CMake.
+# Examples:
+#    * ./build_all.sh -DCMAKE_BUILD_TYPE=Release
+#    * ./build_all.sh VERBOSE=1
+#
+# Written by Tiffany Huang, 12/14/2016
+# Updated by Artem Artemev, 25/06/2017
+
+
+# Go into the directory where this bash script is contained.
+cd `dirname $0`
+
+numproc=$([[ $(uname) == 'Darwin' ]] && sysctl -n hw.physicalcpu_max || nproc)
+
+# Compile code.
+mkdir -p build
+cd build
+cmake ..
+make -j ${numproc} $*
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Written by Artem Artemev, 20/06/2016
+
+# Run particle filter
+cd ./build
+./mpc
